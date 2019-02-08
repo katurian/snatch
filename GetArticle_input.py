3b00d8ca418a3af71d57d6c4db0a9c6694234045
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2019-02-08 17:00:39 -0500

    Update and rename get_input.py to GetArticle_input.py

--- GetArticle_input.py
+++ GetArticle_input.py
@@ -1,16 +1,14 @@
 from newspaper import Article
 
-def GetArticle_in():
+def GetArticle():
     url = input("Enter the article's url: ")
     url = url[0:len(url)-1]
-    publication = input("Enter the publication your article is sourced from: ")
     def label(url):
         url_list = []
         for i in range(len(url)):
             if url[i] == "/":
                 url_list.append(i)
-        return url[url_list[len(url_list)-1] + 1:-1]
-
+            return url_list
     article = Article(url)
     article.download()
     article.parse()
@@ -18,9 +16,8 @@ def GetArticle_in():
     label = label(url)
     if label == None:
         label = "notitle"
-    with open(publication + "$" + str(label) + ".txt", "w") as c:
+    with open(str(url[label[1] + 5:label[3]-13]) + "$" + str(url[label[len(label)-1] + 1:-1]) + ".txt", "w") as c:
         c.write(text)
-        print("Article is at" + " " + (publication + "$" + str(label) + ".txt"))
+    print("Article is at" + " " + ("$" + str(url[label[len(label)-1] + 1:-1]) + ".txt"))
 
-
-GetArticle_in()
+    GetArticle_in()
