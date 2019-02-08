4e819ed08be479c953578b331c403dda04edc0a5
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2019-02-08 17:28:53 -0500

    Add files via upload

---
+++ GetArticles.py
@@ -0,0 +1,34 @@
+import feedparser
+from newspaper import Article
+
+def getURLs(feed):
+    links = []
+    d = feedparser.parse(feed)
+    for l in range(0, len(d['entries'])-1):
+        link = d['entries'][l]['link']
+        links.append(link)
+    return links
+
+def GetArticles(feed):
+    rss_links = getURLs(feed)
+    for i in range(0, len(rss_links)):
+        url = rss_links[i]
+        def label(url):
+            url_list = []
+            for i in range(len(url)):
+                if url[i] == "/":
+                    url_list.append(i)
+            return url_list
+        article = Article(url)
+        article.download()
+        article.parse()
+        text = article.text
+        label = label(url)
+        with open(str(url[label[1] + 1:label[2]]) + "$" + str(url[label[len(label) - 1] + 1:-1]) + ".txt", "w") as c:
+            c.write(text)
+        print("Article is at" + " " + (str(url[label[1] + 1:label[2]]) + "$" + str(url[label[len(label) - 1] + 1:-1]) + ".txt"))
+
+
+GetArticles("https://news.google.com/rss/search?um=1&q=rick+perry&hl=en-US&gl=US&ceid=US:en")
+
+
