e6bed7b692a1e2a7978b142369da7c85435b1776
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2019-02-07 20:38:34 -0500

    Update get.py

--- get.py
+++ get.py
@@ -5,21 +5,26 @@ import requests
 
 
 def GetArticle(url, publication):
+    def label(url):
+        count = 0
+        for i in range(len(url)):
+            if url[i] == "/":
+                count = count + 1
+            if count == 5:
+                return url[i + 1:-1]
+    if publication.lower() == "nyt" or publication.lower() == "new york times" or publication.lower() == "the new york times" or publication.lower() == "atlantic" or publication.lower() == "the atlantic":
+        print("GetArticle() cannot retrieve all article content for this publication.")
     article = Article(url)
     article.download()
     article.parse()
     html = requests.get(url).text
     text = article.text
-    title = article.title
-    date = str(article.publish_date)[0:10]
-    authors = article.authors
-    with open("raw" + "_" + publication + "_" + date + ".txt", "w") as c:
+    label = label(url)
+    if label == None:
+        label = "succ"
+    with open(publication + "_" + str(label) + ".txt", "w") as c:
         c.write(text)
-        print("Article is at" + " " + ("raw" + "_" + publication + "_" + date + ".txt"))
-    image = article.top_image
-    video = article.movies
-    keywords = article.nlp()
-    summary = article.summary
+        print("Article is at" + " " + (publication + "_" + str(label) + ".txt"))
 
-GetArticle('https://www.vox.com/future-perfect/2019/2/7/18215586/india-man-suing-parents-giving-birth-antinatalism-raphael-samuel', 'vox')
 
+GetArticle('https://www.bbc.com/news/world-us-canada-47166938', 'bbc')
