9146d2b71102cd17e302c3fc36fd9372a219acf1
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2019-02-08 16:57:39 -0500

    Update get.py

--- get.py
+++ get.py
@@ -1,12 +1,12 @@
 from newspaper import Article
 
-def GetArticle(url, publication):
+def GetArticle(url):
     def label(url):
         url_list = []
         for i in range(len(url)):
             if url[i] == "/":
                 url_list.append(i)
-        return url[url_list[len(url_list)-1] + 1:-1]
+            return url_list
     article = Article(url)
     article.download()
     article.parse()
@@ -14,9 +14,9 @@ def GetArticle(url, publication):
     label = label(url)
     if label == None:
         label = "notitle"
-    with open(publication + "$" + str(label) + ".txt", "w") as c:
+    with open(str(url[label[1] + 5:label[3]-13]) + "$" + str(url[label[len(label)-1] + 1:-1]) + ".txt", "w") as c:
         c.write(text)
-        print("Article is at" + " " + (publication + "$" + str(label) + ".txt"))
+    print("Article is at" + " " + ("$" + str(url[label[len(label)-1] + 1:-1]) + ".txt"))
 
 
-GetArticle('https://www.nytimes.com/2001/09/12/us/us-attacked-hijacked-jets-destroy-twin-towers-and-hit-pentagon-in-day-of-terror.html', 'nyt')
+GetArticle('https://www.nytimes.com/2001/09/12/us/us-attacked-hijacked-jets-destroy-twin-towers-and-hit-pentagon-in-day-of-terror.html')
