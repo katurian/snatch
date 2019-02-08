abdadad4e6675afd987ff19519f383b4c4ac49b1
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2019-02-08 17:21:27 -0500

    Update GetArticle.py

--- GetArticle.py
+++ GetArticle.py
@@ -12,11 +12,9 @@ def GetArticle(url):
     article.parse()
     text = article.text
     label = label(url)
-    if label == None:
-        label = "notitle"
-    with open(str(url[label[1] + 5:label[3]-13]) + "$" + str(url[label[len(label)-1] + 1:-1]) + ".txt", "w") as c:
+    with open(str(url[label[1] + 1:label[2]]) + "$" + str(url[label[len(label)-1] + 1:-1]) + ".txt", "w") as c:
         c.write(text)
-    print("Article is at" + " " + ("$" + str(url[label[len(label)-1] + 1:-1]) + ".txt"))
+    print("Article is at" + " " + (str(url[label[1] + 1:label[2]]) + "$" + str(url[label[len(label)-1] + 1:-1]) + ".txt"))
 
 
 GetArticle('https://www.nytimes.com/2001/09/12/us/us-attacked-hijacked-jets-destroy-twin-towers-and-hit-pentagon-in-day-of-terror.html')
