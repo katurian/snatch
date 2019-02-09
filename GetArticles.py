f63ceefaa292b139bcbe03742e66b965f2e22ee9
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2019-02-08 23:47:08 -0500

    Update GetArticles.py

--- GetArticles.py
+++ GetArticles.py
@@ -4,21 +4,23 @@ from newspaper import Article
 def getURLs(feed):
     links = []
     d = feedparser.parse(feed)
-    for l in range(0, len(d['entries'])-1):
+    for l in(d['entries']:
         link = d['entries'][l]['link']
         links.append(link)
     return links
 
+def label(url):
+    url_list = []
+    for i in range(len(url)):
+        if url[i] == "/":
+            url_list.append(i)
+        return url_list
+    
+    
 def GetArticles(feed):
     rss_links = getURLs(feed)
     for i in range(0, len(rss_links)):
         url = rss_links[i]
-        def label(url):
-            url_list = []
-            for i in range(len(url)):
-                if url[i] == "/":
-                    url_list.append(i)
-            return url_list
         article = Article(url)
         article.download()
         article.parse()
