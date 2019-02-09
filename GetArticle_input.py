cdd05d7e56a312c4b77bffa27cc1552455b82330
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2019-02-08 23:47:40 -0500

    Update GetArticle_input.py

--- GetArticle_input.py
+++ GetArticle_input.py
@@ -1,14 +1,15 @@
 from newspaper import Article
 
+def label(url):
+    url_list = []
+    for i in range(len(url)):
+        if url[i] == "/":
+            url_list.append(i)
+        return url_list
+    
 def GetArticle_in():
     url = input("Enter the article's url: ")
     url = url[0:len(url)-1]
-    def label(url):
-        url_list = []
-        for i in range(len(url)):
-            if url[i] == "/":
-                url_list.append(i)
-        return url_list
     article = Article(url)
     article.download()
     article.parse()
