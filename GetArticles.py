03142f0ceea0ab3de4fafc2c62a3a37c1b38e547
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2019-02-09 00:24:08 -0500

    Update GetArticles.py

--- GetArticles.py
+++ GetArticles.py
@@ -4,7 +4,7 @@ from newspaper import Article
 def getURLs(feed):
     links = []
     d = feedparser.parse(feed)
-    for l in(d['entries']:
+    for l in range(0, len(d['entries'])-1):
         link = d['entries'][l]['link']
         links.append(link)
     return links
@@ -14,8 +14,8 @@ def label(url):
     for i in range(len(url)):
         if url[i] == "/":
             url_list.append(i)
-        return url_list    
-    
+    return url_list
+
 def GetArticles(feed):
     rss_links = getURLs(feed)
     for i in range(0, len(rss_links)):
@@ -24,12 +24,10 @@ def GetArticles(feed):
         article.download()
         article.parse()
         text = article.text
-        label = label(url)
-        with open(str(url[label[1] + 1:label[2]]) + "$" + str(url[label[len(label) - 1] + 1:-1]) + ".txt", "w") as c:
+        parsed = label(url)
+        with open(str(url[parsed[1] + 1:parsed[2]]) + "$" + str(url[parsed[len(parsed) - 1] + 1:-1]) + ".txt", "w") as c:
             c.write(text)
-        print("Article is at" + " " + (str(url[label[1] + 1:label[2]]) + "$" + str(url[label[len(label) - 1] + 1:-1]) + ".txt"))
+        print("Article is at" + " " + (str(url[parsed[1] + 1:parsed[2]]) + "$" + str(url[parsed[len(parsed) - 1] + 1:-1]) + ".txt"))
 
 
-GetArticles("https://news.google.com/rss/search?um=1&q=rick+perry&hl=en-US&gl=US&ceid=US:en")
-
-
+GetArticles("https://www.economist.com/leaders/rss.xml")
